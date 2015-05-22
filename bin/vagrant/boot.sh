4b547eaa0499c0aa7efa4a0bb3e6ef7ad870f7fa
aeneasr <aeneas.rekkas@serlo.org>
2015-05-22 23:14:36 +0200

    Fixed typos

--- bin/vagrant/boot.sh
+++ bin/vagrant/boot.sh
@@ -7,7 +7,7 @@ rm /vagrant/src/data/* -rf
 # sudo su - www-data -c "(cd /vagrant/src/assets;npm cache clean)"
 # sudo su - www-data -c "(cd /vagrant/src/assets;npm install)"
 # sudo su - www-data -c "(cd /vagrant/src/assets;npm update)"
-pm2 start /vagrant/src/assets/node_modules/athene2-editor/server/server.js --node-args="----expose_gc --gc_global"
+pm2 start /vagrant/src/assets/node_modules/athene2-editor/server/server.js --node-args="--expose_gc --gc_global"
 # sudo su - www-data -c "(cd /vagrant/src/assets;bower cache clean)"
 # sudo su - www-data -c "(cd /vagrant/src/assets;bower --config.analytics=false update)"
 # sudo su - www-data -c "(cd /vagrant/;COMPOSER_PROCESS_TIMEOUT=5600 php composer.phar install -o)"
--- bin/vagrant/clean-ui.sh
+++ bin/vagrant/clean-ui.sh
@@ -8,4 +8,4 @@ rm -R /vagrant/src/assets/source/bower_components/*
 cd /vagrant/src/assets/;npm install
 (cd /vagrant/src/assets;bower --config.analytics=false install)
 (cd /vagrant/src/assets;grunt build)
-pm2 start /vagrant/src/assets/node_modules/athene2-editor/server/server.js --node-args="----expose_gc --gc_global"
+pm2 start /vagrant/src/assets/node_modules/athene2-editor/server/server.js --node-args="--expose_gc --gc_global"
