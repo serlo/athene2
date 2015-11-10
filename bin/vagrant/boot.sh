fc43fa910cdac5046d93df9c322cc75f8358bd3f
Jonas Keinholz <jonas@keinholz.com>
2015-11-10 17:21:59 +0100

    Fix grunt build

--- bin/vagrant/boot.sh
+++ bin/vagrant/boot.sh
@@ -2,14 +2,7 @@
 
 sudo cp -R /vagrant/dist/solr/* /etc/solr/conf/
 sudo service tomcat6 restart
-# mkdir /vagrant/src/public/assets
 rm /vagrant/src/data/* -rf
-# sudo su - www-data -c "(cd /vagrant/src/assets;npm cache clean)"
-# sudo su - www-data -c "(cd /vagrant/src/assets;npm install)"
-# sudo su - www-data -c "(cd /vagrant/src/assets;npm update)"
-pm2 start /vagrant/src/assets/athene2-editor/server/server.js --node-args="--expose_gc --gc_global" -i max
-# sudo su - www-data -c "(cd /vagrant/src/assets;bower cache clean)"
-# sudo su - www-data -c "(cd /vagrant/src/assets;bower --config.analytics=false update)"
-# sudo su - www-data -c "(cd /vagrant/;COMPOSER_PROCESS_TIMEOUT=5600 php composer.phar install -o)"
-# sudo su - www-data -c "(cd /vagrant/;COMPOSER_PROCESS_TIMEOUT=5600 php composer.phar update -o)"
-cd /vagrant/src/assets && grunt build
+cd /vagrant/src/assets
+pm2 start athene2-editor/server/server.js --node-args="--expose_gc --gc_global" -i max
+grunt build
