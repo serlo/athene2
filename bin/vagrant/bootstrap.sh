d89cc001540f6f29585f9b3cc70393ba307ef21f
Aeneas Rekkas <aeneas.rekkas@serlo.org>
2015-12-04 18:39:00 +0100

    vagrant: php7

--- bin/vagrant/bootstrap.sh
+++ bin/vagrant/bootstrap.sh
@@ -11,6 +11,9 @@ echo "phpmyadmin phpmyadmin/mysql/admin-pass password athene2" | debconf-set-sel
 echo "phpmyadmin phpmyadmin/mysql/app-pass password athene2" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
 
+# php7
+sudo add-apt-repository -y ppa:ondrej/php > /dev/null 2>&1
+
 # Install basic stuff
 apt-get -y update
 apt-get install -y python-software-properties python g++ make python-software-properties
@@ -18,8 +21,7 @@ apt-get install -y apache2 mysql-server-5.5 git
 apt-get install -y language-pack-de-base language-pack-en-base inotify-tools
 
 # Install php
-apt-get install -y libapache2-mod-php5 php5 php5-intl php5-mysql php5-curl php-pear phpmyadmin
-apt-get install -y php5-xdebug php5-cli php-apc php-xml-parser
+apt-get install -y libapache2-mod-fcgid php7.0 php7.0-common php7.0-fpm php7.0-opcache php7.0-intl  php7.0-cli php7.0-mysql php7.0-curl libapache2-mod-php7.0 php-xml-parser php-pear phpmyadmin
 apt-get install -y solr-tomcat
 
 # Install nodejs related stuff
@@ -43,12 +45,11 @@ echo "<VirtualHost *:80>
 
 	DocumentRoot /var/www/src/public/
 
-	# ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9000/vagrant/src/public
-
 	<Directory />
 		Options FollowSymLinks
 		AllowOverride None
 	</Directory>
+
 	<Directory /var/www/src/public>
 		Options Indexes FollowSymLinks MultiViews
 		AllowOverride All
@@ -64,10 +65,19 @@ echo "<VirtualHost *:80>
 		Order allow,deny
 		allow from all
 	</Directory>
+
 </VirtualHost>" > /etc/apache2/sites-available/athene2.conf
+
+echo "
+<Ifmodule mod_fcgid.c>
+    AddHandler fcgid-script .php
+    Options +ExecCGI
+</IfModule>" > /etc/apache2/mods-enabled/fastcgi.conf
+
 a2ensite athene2
 a2dissite 000-default
 
+# Install boot script at boot time
 echo "
 # Listen and start after the vagrant-mounted event
 start on vagrant-mounted
@@ -77,7 +87,7 @@ exec su vagrant -c /vagrant/bin/vagrant/boot.sh >> /home/vagrant/boot.log
 " > /etc/init/athene2startup.conf
 
 # Xdebug fix
-sed -i "$ a\xdebug.max_nesting_level = 500" /etc/php5/apache2/php.ini
+sed -i "$ a\xdebug.max_nesting_level = 500" /etc/php7/apache2/php.ini
 
 # Change Apache User to vagrant
 sed -i "s/www-data/vagrant/g" /etc/apache2/envvars
@@ -92,7 +102,7 @@ chown -R vagrant:vagrant /vagrant
 chmod -R 777 /vagrant
 
 # Enable Apache mods
-a2enmod rewrite proxy proxy_fcgi headers
+a2enmod rewrite proxy proxy_fcgi headers actions
 
 # MySQL
 sed -i "s/bind-address.*=.*/bind-address=0.0.0.0/" /etc/mysql/my.cnf
@@ -103,13 +113,13 @@ sed -i "s/bind-address.*=.*/bind-address=0.0.0.0/" /etc/mysql/my.cnf
 # rm cron
 
 # Php hacks
-sed -i "s/\;pcre\.backtrack\_limit=100000/pcre\.backtrack\_limit=10000/" /etc/php5/cli/php.ini
-sed -i "s/\;pcre\.backtrack\_limit=100000/pcre\.backtrack\_limit=10000/" /etc/php5/apache2/php.ini
-sed -i "s/\memory\_limit = 128M/memory\_limit = 1024M/" /etc/php5/apache2/php.ini
-sed -i "s/\upload\_max\_filesize = .*M/upload\_max\_filesize = 128M/" /etc/php5/apache2/php.ini
-sed -i "s/\post\_max\_size = .*M/post\_max\_size = 128M/" /etc/php5/apache2/php.ini
-echo "apc.enabled = 1" >> /etc/php5/cli/php.ini
-echo "apc.enable_cli = 1" >> /etc/php5/cli/php.ini
+sed -i "s/\;pcre\.backtrack\_limit=100000/pcre\.backtrack\_limit=10000/" /etc/php7/cli/php.ini
+sed -i "s/\;pcre\.backtrack\_limit=100000/pcre\.backtrack\_limit=10000/" /etc/php7/apache2/php.ini
+sed -i "s/\memory\_limit = 128M/memory\_limit = 1024M/" /etc/php7/apache2/php.ini
+sed -i "s/\upload\_max\_filesize = .*M/upload\_max\_filesize = 128M/" /etc/php7/apache2/php.ini
+sed -i "s/\post\_max\_size = .*M/post\_max\_size = 128M/" /etc/php7/apache2/php.ini
+echo "apc.enabled = 1" >> /etc/php7/cli/php.ini
+echo "apc.enable_cli = 1" >> /etc/php7/cli/php.ini
 
 su vagrant - -c "(cd /vagrant/;COMPOSER_PROCESS_TIMEOUT=5600 php composer.phar install)"
 
