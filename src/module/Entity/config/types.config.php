614bfe97a72b85d835d88a1c59dfa5057a932e54
Jonas Keinholz <jonas@keinholz.com>
2016-02-27 11:51:04 +0100

    WIP Add entity type `choice-exercise`

--- src/module/Entity/config/types.config.php
+++ src/module/Entity/config/types.config.php
@@ -29,6 +29,53 @@ return [
                 ]
 
             ],
+            'choice-exercise' => [
+                'title' => 'id',
+                'description' => 'content',
+                'components' => [
+                    'repository' => [
+                        // no form yet
+                        'fields' => [
+                            'type', // MC or SC
+                            'content',
+                            'changes'
+                        ]
+                    ]
+                ],
+                'link' => [
+                    'children' => [
+                        'choice-exercise-choice' => [
+                            'multiple' => true
+                        ],
+                        'text-hint' => [
+                            'multiple' => false
+                        ]
+                    ]
+                ],
+                'license' => [],
+                'taxonomy' => []
+            ],
+            'choice-exercise-choice' => [
+                'title' => 'id',
+                'description' => 'answer',
+                'components' => [
+                    'repository' => [
+                        // no form yet
+                        'fields' => [
+                            'correct', // true or false
+                            'content',
+                            'changes'
+                        ]
+                    ]
+                ],
+                'link' => [
+                    'children' => [
+                        'text-hint' => [
+                            'multiple' => false
+                        ]
+                    ]
+                ]
+            ],
             'text-exercise'         => [
                 'title'       => 'id',
                 'description' => 'content',
@@ -238,7 +285,7 @@ return [
                     ],
                     'license'    => []
                 ]
-            ]
+            ],
         ]
     ]
 ];
