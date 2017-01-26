3a149580447b43295a166fe110f98707787eb352
Knorrke <knorr.b@gmx.de>
2017-01-26 11:35:46 +0100

    trashform

---
+++ src/module/Uuid/src/Uuid/View/Helper/FormHelper.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Athene2 - Advanced Learning Resources Manager
+ *
+ * @author    Aeneas Rekkas (aeneas.rekkas@serlo.org)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @link      https://github.com/serlo-org/athene2 for the canonical source repository
+ * @copyright Copyright (c) 2013-2014 Gesellschaft für freie Bildung e.V. (http://www.open-education.eu/)
+ */
+namespace Uuid\View\Helper;
+
+use UUid\Form\PurgeForm;
+use Uuid\Form\TrashForm;
+use Zend\View\Helper\AbstractHelper;
+use Zend\Form\Form;
+
+class FormHelper extends AbstractHelper
+{
+    public function __invoke() {
+        return $this;
+    }
+
+    public function getForm($action, $objectID)
+    {
+        if ($action === 'trash') {
+            $url = $this->getView()->url('uuid/trash', ['id' => $objectID]);
+            /** @var Form $form */
+            $form = new TrashForm($objectID, $url);
+            $form->setAttribute('action', $url);
+            return $form;
+        } else if ($action === 'purge') {
+            $url = $this->getView()->url('uuid/purge', ['id' => $objectID]);
+            /** @var Form $form */
+            $form = new PurgeForm($objectID, $url);
+            $form->setAttribute('action', $url);
+            return $form;
+        }
+    }
+}
