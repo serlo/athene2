f952801299cc80d5ce7756f04e63c2ae26d36fef
Knorrke <knorr.b@gmx.de>
2017-03-08 16:56:56 +0100

    added form for removing ads

---
+++ src/module/Ads/src/Ads/Form/RemoveAdForm.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Athene2 - Advanced Learning Resources Manager
+ *
+ * @author    Benjamin Knorr (benjamin@serlo.org)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @link      https://github.com/serlo-org/athene2 for the canonical source repository
+ */
+namespace Ads\Form;
+
+use Common\Form\Element\CsrfToken;
+use Zend\Form\Element\Submit;
+use Zend\Form\Form;
+
+class RemoveAdForm extends Form
+{
+    function __construct()
+    {
+        parent::__construct('remove-page');
+        $this->setAttribute('method', 'post');
+
+        $this->add(new CsrfToken('csrf'));
+
+        $this->add(
+            (new Submit('submit'))->setValue('Remove Ad')->setAttribute('class', 'btn btn-success pull-right')
+        );
+    }
+}
