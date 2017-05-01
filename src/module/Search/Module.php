5187a549c088cb595a205d90416b87149546aefc
Knorrke <Knorrke@users.noreply.github.com>
2017-05-01 20:31:14 +0200

    closes #479 - replace search with google custom search (#508)

--- src/module/Search/Module.php
+++ src/module/Search/Module.php
@@ -12,11 +12,6 @@ use Zend\Mvc\MvcEvent;
 
 class Module
 {
-    public static $listeners = [
-        'Search\Listener\RepositoryManagerListener',
-        'Search\Listener\TaxonomyManagerListener',
-        'Search\Listener\UuidManagerListener',
-    ];
 
     public function getConfig()
     {
@@ -43,22 +38,5 @@ class Module
         }
 
         return $autoloader;
-    }
-
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager = $e->getApplication()->getEventManager();
-        $eventManager->attach(MvcEvent::EVENT_DISPATCH, array($this, 'onDispatchRegisterListeners'), 1000);
-    }
-
-    public function onDispatchRegisterListeners(MvcEvent $e)
-    {
-        $eventManager       = $e->getApplication()->getEventManager();
-        $sharedEventManager = $eventManager->getSharedManager();
-        foreach (self::$listeners as $listener) {
-            $sharedEventManager->attachAggregate(
-                $e->getApplication()->getServiceManager()->get($listener)
-            );
-        }
     }
 }
