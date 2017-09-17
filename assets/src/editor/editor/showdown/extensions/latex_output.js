00d7326cfec762398fb454b2e763d3e4f0f7f6c0
Jonas Keinholz <jonas@keinholz.com>
2017-09-17 12:42:52 +0200

    assets: opt in for prerelease assets using cookie (#574)

---
+++ assets/src/editor/editor/showdown/extensions/latex_output.js
@@ -0,0 +1,62 @@
+/*global define, module, window*/
+(function() {
+  var serloSpecificCharsToEncode,
+    latexoutput = function() {
+      return [
+        {
+          type: 'output',
+          filter: function(text) {
+            return encodeSerloSpecificChars(text);
+          }
+        }
+      ];
+    };
+
+  serloSpecificCharsToEncode = (function() {
+    var regexp,
+      chars = ['*', '`', '_', '{', '}', '[', ']', '&lt;', '\\'],
+      replacements = {},
+      l = chars.length,
+      i = 0;
+
+    for (; i < l; i++) {
+      replacements['' + i] = chars[i];
+    }
+
+    regexp = new RegExp('§LT([0-9])', 'gm');
+
+    function replace(whole, match) {
+      return replacements[parseInt(match)] || match;
+    }
+
+    return {
+      regexp: regexp,
+      replace: replace
+    };
+  })();
+
+  function encodeSerloSpecificChars(text) {
+    return text.replace(
+      serloSpecificCharsToEncode.regexp,
+      serloSpecificCharsToEncode.replace
+    );
+  }
+
+  // Client-side export
+  if (typeof define === 'function' && define.amd) {
+    define('showdown_latex_output', ['showdown'], function(Showdown) {
+      Showdown.extensions = Showdown.extensions || {};
+      Showdown.extensions.latexoutput = latexoutput;
+    });
+  } else if (
+    typeof window !== 'undefined' &&
+    window.Showdown &&
+    window.Showdown.extensions
+  ) {
+    window.Showdown.extensions.latexoutput = latexoutput;
+  }
+  // Server-side export
+  if (typeof module !== 'undefined') {
+    module.exports = latexoutput;
+  }
+})();
