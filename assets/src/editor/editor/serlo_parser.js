f3ad7f547ff888b42c4316356a7a8dcd38e24ed4
Aeneas <arekkas@users.noreply.github.com>
2017-09-17 12:57:30 +0200

    assets: opt in for prerelease assets using cookie (#577)

---
+++ assets/src/editor/editor/serlo_parser.js
@@ -0,0 +1,14 @@
+/*global define*/
+var Parser = function() {};
+
+Parser.prototype.setConverter = function(converter, convertFunctionName) {
+  this.converter = function(value) {
+    return converter[convertFunctionName](value);
+  };
+};
+
+Parser.prototype.parse = function(value) {
+  return this.converter ? this.converter(value) : value;
+};
+
+export default Parser;
---
+++ assets/webpack.config.base.js
@@ -0,0 +1,59 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  target: 'web',
+  entry: {
+    main: './src/main',
+    editor: './src/editor'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loader: require.resolve('babel-loader')
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ],
+          fallback: 'style-loader',
+          publicPath: './'
+        })
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff2?)$/,
+        loader: 'file-loader?name=[name].[ext]'
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: 'file-loader?name=[name].[ext]'
+      }
+    ]
+  },
+  externals: [require('webpack-require-http')],
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons',
+      filename: 'commons.js'
+    })
+  ]
+};
