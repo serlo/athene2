61c567f09e7b363498d1ccaa3133a3c2500853ac
aeneasr <aeneas.rekkas@serlo.org>
2017-10-12 14:51:27 +0200

    Implement donation form to www.serlo.org/spenden
    Closes #543

---
+++ src/public/spenden.php
@@ -0,0 +1,6 @@
+<html>
+<head>Serlo.org - Spenden</head>
+<body>
+<script type="text/javascript" src="https://secure.fundraisingbox.com/app/paymentJS?hash=gtfhzy5ijix2yy35"></script><noscript>Bitte Javascript aktivieren</noscript><a target="_blank" href="http://www.fundraisingbox.com"><img border="0" style="border: 0 !important" src="https://secure.fundraisingbox.com/images/FundraisingBox-Logo-Widget.png" alt="FundraisingBox Logo" /></a>
+</body>
+</html>
