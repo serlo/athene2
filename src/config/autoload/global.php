ca9c8ea330f73bd253fdb3d461e2d71d242666c0
Jonas Keinholz <jonas@keinholz.com>
2018-09-06 11:27:55 +0200

    fix: assets_host

--- src/config/autoload/global.php
+++ src/config/autoload/global.php
@@ -68,7 +68,7 @@ return [
             'previous_uri_query_key'         => 'redir',
         ],
     ],
-    'assets_host'     => 'http://localhost:8081/',
+    'assets_host'     => 'https://package-registry.serlo.org/athene2-assets@2/',
     'editor_renderer' => [
         'url' => 'https://europe-west1-serlo-assets.cloudfunctions.net/editor-renderer',
     ],
