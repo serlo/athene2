ebacd3b8d6e805518ad24914d89d8b3544c44c18
Knorrke <Knorrke@users.noreply.github.com>
2018-09-07 06:27:03 +0200

    fix: add alias path for entity children using parent (#698)

--- src/module/Entity/src/Entity/Provider/TokenProvider.php
+++ src/module/Entity/src/Entity/Provider/TokenProvider.php
@@ -8,9 +8,11 @@
  */
 namespace Entity\Provider;
 
+use Doctrine\Common\Collections\Collection;
 use Entity\Entity\EntityInterface;
 use Normalizer\NormalizerAwareTrait;
 use Normalizer\NormalizerInterface;
+use Taxonomy\Entity\TaxonomyTermInterface;
 use Taxonomy\Strategy\BranchDecisionMakerStrategy;
 use Taxonomy\Strategy\ShortestBranchDecisionMaker;
 use Token\Provider\AbstractProvider;
@@ -42,13 +44,18 @@ class TokenProvider extends AbstractProvider implements ProviderInterface
         $path = $this->getObject()->getId();
 
         if ($term) {
-            $path = [];
-            while ($term->hasParent()) {
-                $path[] = $term->getName();
-                $term = $term->getParent();
+            $path = $this->createPathFromTerm($term);
+        } else {
+            /** @var Collection|EntityInterface[] $parents */
+            $parents = $this->getObject()->getParents('link');
+            if ($parents->count()) {
+                $term = $this->strategy->findBranch($parents->first()->getTaxonomyTerms());
+                if ($term) {
+                    $path = $this->createPathFromTerm($term);
+                    $normalizedParent = $this->getNormalizer()->normalize($parents->first());
+                    $path = $path . '/' . $normalizedParent->getTitle();
+                }
             }
-            $path = array_reverse($path);
-            $path = implode('/', $path);
         }
 
         $normalized = $this->getNormalizer()->normalize($this->getObject());
@@ -59,6 +66,17 @@ class TokenProvider extends AbstractProvider implements ProviderInterface
             'title' => $title,
             'id'    => $this->getObject()->getId(),
         ];
+    }
+
+    private function createPathFromTerm(TaxonomyTermInterface $term) {
+        $path = [];
+        while ($term->hasParent()) {
+            $path[] = $term->getName();
+            $term = $term->getParent();
+        }
+        $path = array_reverse($path);
+        $path = implode('/', $path);
+        return $path;
     }
 
     protected function isValid(EntityInterface $object)
