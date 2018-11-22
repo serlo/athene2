29005e96d6b2f616f88b582c8eb7b7aac3db574a
Marinus <marinus.maier@gmail.com>
2018-11-22 16:56:20 +0100

    CI Test 6 - tests correct again

--- src/module/Blog/test/BlogTest/Entity/PostTest.php
+++ src/module/Blog/test/BlogTest/Entity/PostTest.php
@@ -19,7 +19,7 @@ class PostTest extends AbstractGetterSetterTest
     protected function getData()
     {
         return array(
-            "authora" => $this->createMock("User\Entity\UserInterface"),
+            "author" => $this->createMock("User\Entity\UserInterface"),
             "blog" => $this->createMock("Taxonomy\Entity\TaxonomyTermInterface"),
             "title" => "title",
             "content" => "content",
