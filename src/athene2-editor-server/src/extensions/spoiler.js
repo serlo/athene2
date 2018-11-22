34b657bdc48c8467d209a27e2810cead693f6ae7
Jonas Keinholz <jonas@keinholz.com>
2018-11-22 13:22:57 +0100

    fix(athene2-editor-server): use same code as on client-side

--- src/athene2-editor-server/src/extensions/spoiler.js
+++ src/athene2-editor-server/src/extensions/spoiler.js
@@ -1,11 +1,33 @@
 /**
+ * This file is part of Athene2 Assets.
+ *
+ * Copyright (c) 2017-2018 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2013-2018 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/athene2-assets for the canonical source repository
+ */
+/* global define */
+/**
  * Serlo Flavored Markdown
  * Spoilers:
  * Transforms ///.../// blocks into spoilers
  **/
-var spoiler = function(converter) {
-  var filter,
-    findSpoilers = new RegExp(/^<p>=,sp. (.*)<\/p>([\s\S]*?)<p>=,sp.<\/p>/gm)
+var spoiler = function() {
+  var filter
+  var findSpoilers = new RegExp(/^<p>=,sp. (.*)<\/p>([\s\S]*?)<p>=,sp.<\/p>/gm)
 
   filter = function(text) {
     return text.replace(findSpoilers, function(original, title, content) {
--- src/athene2-editor-server/src/extensions/spoiler_prepare.js
+++ src/athene2-editor-server/src/extensions/spoiler_prepare.js
@@ -1,11 +1,33 @@
 /**
+ * This file is part of Athene2 Assets.
+ *
+ * Copyright (c) 2017-2018 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2013-2018 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/athene2-assets for the canonical source repository
+ */
+/* global define */
+/**
  * Serlo Flavored Markdown
  * Spoilers:
  * Transforms ///.../// blocks into spoilers
  **/
-var spoilerprepare = function(converter) {
-  var filter,
-    findSpoilers = new RegExp(/^\/\/\/ (.*)\n([\s\S]*?)(\n|\r)+\/\/\//gm)
+var spoilerprepare = function() {
+  var filter
+  var findSpoilers = new RegExp(/^\/\/\/ (.*)\n([\s\S]*?)(\n|\r)+\/\/\//gm)
 
   filter = function(text) {
     // convert all "///"s into "=,sp."s
@@ -20,6 +42,20 @@ var spoilerprepare = function(converter) {
       filter: filter
     }
   ]
+}
+
+// Client-side export
+if (typeof define === 'function' && define.amd) {
+  define('showdown_spoiler_prepare', ['showdown'], function(Showdown) {
+    Showdown.extensions = Showdown.extensions || {}
+    Showdown.extensions.spoilerprepare = spoilerprepare
+  })
+} else if (
+  typeof window !== 'undefined' &&
+  window.Showdown &&
+  window.Showdown.extensions
+) {
+  window.Showdown.extensions.spoilerprepare = spoilerprepare
 }
 
 module.exports = spoilerprepare
