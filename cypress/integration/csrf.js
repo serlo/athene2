cbee5ee70aa515b223609898afa4a32b2dac5821
Knorrke <knorr.b@gmx.de>
2018-12-19 12:11:13 +0100

    e2e tests for csrf token

---
+++ cypress/integration/csrf.js
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Athene2.
+ *
+ * Copyright (c) 2013-2018 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2013-2018 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/athene2 for the canonical source repository
+ */
+/// <reference types="Cypress" />
+
+context('Csrf', () => {
+  it('de.serlo.localhost has a csrf token on window.csrf', () => {
+    cy.visit('http://de.serlo.localhost:4567/')
+    cy.window().should('have.property', 'csrf')
+  })
+  it('home has the same token on window.csrf as /auth/login', () => {
+    cy.visit('http://de.serlo.localhost:4567/')
+    cy.window().its('csrf').then(csrf => {
+      cy.visit('http://de.serlo.localhost:4567/auth/login')
+      cy.window().its('csrf').should('equal', csrf)
+    })
+  })
+  it('window.csrf is the same token as the form field', () => {
+    cy.visit('http://de.serlo.localhost:4567/auth/login')
+    cy.window().its('csrf').then(csrf => {
+      cy.get('input[name="csrf"]').should('have.value', csrf)
+    })
+  })
+})
