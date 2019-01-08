e0ed4fd9cd11af53e0631091fd4b3e05d322815f
Knorrke <knorr.b@gmx.de>
2019-01-08 13:23:03 +0100

    add unit tests for mailman

--- src/module/Mailman/src/Mailman/Model/Mail.php
+++ src/module/Mailman/src/Mailman/Model/Mail.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of Athene2.
  *
- * Copyright (c) 2013-2019 Serlo Education e.V.
+ * Copyright (c) 2013-2018 Serlo Education e.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License
@@ -16,9 +16,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @copyright Copyright (c) 2013-2019 Serlo Education e.V.
+ * @copyright Copyright (c) 2013-2018 Serlo Education e.V.
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/serlo-org/athene2 for the canonical source repository
  */
+namespace Mailman\Model;
 
-echo $this->translate("How to get started on Serlo!");
+class Mail implements MailInterface {
+    protected $subject;
+    protected $body;
+    protected $plain;
+
+    public function __construct($subject, $body, $plain)
+    {
+        $this->subject = $subject;
+        $this->body = $body;
+        $this->plain = $plain;
+    }
+
+    public function getSubject() {
+        return $this->subject;
+    }
+
+    public function getHtmlBody() {
+        return $this->body;
+    }
+
+    public function getPlainBody() {
+        return $this->plain;
+    }
+}
