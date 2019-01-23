bf7756f21592511c4f05c33bec6df4630b8e6919
Jonas Keinholz <jonas@keinholz.com>
2019-01-23 16:02:41 +0100

    feat(FeatureFlags): initial commit

---
+++ src/module/FeatureFlags/src/FeatureFlags/Factory/ServiceFactory.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of Athene2.
+ *
+ * Copyright (c) 2013-2019 Serlo Education e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @copyright Copyright (c) 2013-2019 Serlo Education e.V.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      https://github.com/serlo-org/athene2 for the canonical source repository
+ */
+namespace FeatureFlags\Factory;
+
+use FeatureFlags\Service;
+use FeatureFlags\ServiceLoggerInterface;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class ServiceFactory implements FactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     * @return Service
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('Config')['feature_flags'];
+        /**
+         * @var $sentry ServiceLoggerInterface
+         */
+        $sentry = $serviceLocator->get('Log\Sentry');
+
+        return new Service($config, $sentry);
+    }
+}
