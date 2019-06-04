b1101940e99115939fc7edea70bcb878c5a182bb
Jonas Keinholz <git@inyono.dev>
2019-06-04 16:26:02 +0200

    chore: changelog

--- src/config/autoload/global.php
+++ src/config/autoload/global.php
@@ -21,7 +21,7 @@
  * @link      https://github.com/serlo-org/athene2 for the canonical source repository
  */
 return [
-    'version' => '11',
+    'version' => '12',
     'brand' => [
         'instances' => [
             'deutsch' => [
