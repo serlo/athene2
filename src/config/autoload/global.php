56a7bf1c99a6ee314e68a1cf0c906ec7f9128bc8
Jonas Keinholz <git@inyono.dev>
2019-07-30 16:23:41 +0200

    chore: release build 19

--- src/config/autoload/global.php
+++ src/config/autoload/global.php
@@ -21,7 +21,7 @@
  * @link      https://github.com/serlo-org/athene2 for the canonical source repository
  */
 return [
-    'version' => '18',
+    'version' => '19',
     'brand' => [
         'instances' => [
             'deutsch' => [
