8cb1cde2ed0136f01e0744c676db2b1b32f3f69c
Jonas Keinholz <git@inyono.dev>
2019-07-31 16:02:23 +0200

    fix(server): use new inline svg in layout/*/serlo-home

--- src/config/autoload/global.php
+++ src/config/autoload/global.php
@@ -21,7 +21,7 @@
  * @link      https://github.com/serlo-org/athene2 for the canonical source repository
  */
 return [
-    'version' => '19',
+    'version' => '20',
     'brand' => [
         'instances' => [
             'deutsch' => [
